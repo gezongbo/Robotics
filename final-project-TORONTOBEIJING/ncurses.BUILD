edffab5ffa9dbaadf7721bff957ccad9d4ff3d4e
BoBo yeah <2962077246@qq.com>
2023-02-06 16:01:48 +0800

    remove

--- final-project-TORONTOBEIJING/ncurses.BUILD
+++
@@ -1,5 +0,0 @@
-cc_library(
-   name = "main",
-   srcs = ["lib/libncurses.a"],
-   visibility = ["//visibility:public"],
-)
--- final-project-TORONTOBEIJING/tests/trojanmap_test_student.cc
+++
@@ -1,7 +0,0 @@
-#include "gtest/gtest.h"
-#include "src/lib/trojanmap.h"
-
-TEST(TrojanMapStudentTest, Test1) {
-  EXPECT_EQ(true, true);
-}
-
